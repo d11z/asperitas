e9f45049be55e681b4ff7565cfcf12828da1eb6a
Deniz Basegmez <dbas12@gmail.com>
2018-11-04 13:58:38 -0800

    Move font declaration inside App component

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className='app'>
         <Header subreddit='r/all'/>
       </div>
     );
