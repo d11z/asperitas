5258edc9e8b982e25b45d6e731f56fceeeab0f7a
Deniz Basegmez <dbas12@gmail.com>
2018-11-04 17:03:51 -0800

    Update Header tests

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -6,3 +6,10 @@ import Header from './Header';
 it('renders without crashing', () => {
   shallow(<Header/>);
 });
+
+it('renders current subreddit', () => {
+  const subreddit = 'all';
+  const wrapper = shallow(<Header subreddit={subreddit}/>);
+
+  expect(wrapper.find('.header__subreddit').text()).toEqual(`r/${subreddit}`);
+});
