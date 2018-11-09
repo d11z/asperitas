12346ffeb9022f8a94522427ca408ae44d5ce8b5
Deniz Basegmez <dbas12@gmail.com>
2018-11-09 15:54:50 -0800

    Use styled-components babel macro

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import { Route } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import ListView from './ListView';
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
