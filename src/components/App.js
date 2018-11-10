2003719794358d4f88716e571acdfbf4f04bebc9
Deniz Basegmez <dbas12@gmail.com>
2018-11-10 11:27:21 -0800

    Move font from body to * in global styles

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,6 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-  }
-  
-  body {
     font-family: 'IBM Plex Sans', 'Roboto', 'Helvetica Neue', 'Arial', sans-serif;
   }
 `;
