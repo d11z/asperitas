a98f9c2debc6cacffb7b7119345710feaff7e7ed
Deniz Basegmez <dbas12@gmail.com>
2018-11-10 11:17:21 -0800

    Add CommentForm component

---
+++ src/components/CommentForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Field, reduxForm } from 'redux-form';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  flex: 2;
+  background-color: #eee;
+`;
+
+const TextArea = styled(Field)`
+  padding: 8px;
+  border: 1px solid #eee;
+  font-size: 16px;
+`;
+
+const SubmitButton = styled.button`
+  align-self: flex-end;
+  padding: 4px 16px;
+  margin: 4px;
+  font-size: 14px;
+  font-weight: 500;
+  border: 1px solid #eee;
+`;
+
+const CommentForm = props => (
+  <Form>
+    <TextArea
+      name='comment'
+      component='textarea'
+      rows='3'
+      placeholder='enter your comment'
+    />
+    <SubmitButton type='submit'>submit</SubmitButton>
+  </Form>
+);
+
+export default reduxForm({ form: 'comment' })(CommentForm);
---
+++ src/components/CommentForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CommentForm from './CommentForm';
+
+it('renders without crashing', () => {
+  shallow(<CommentForm/>);
+});
