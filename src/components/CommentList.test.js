884d64b628a32678a9beb7f1d3a8e9cbae0b9d94
Deniz Basegmez <dbas12@gmail.com>
2018-11-10 11:15:57 -0800

    Add CommentList component

---
+++ src/components/CommentList.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CommentList from './CommentList';
+
+it('renders without crashing', () => {
+  shallow(<CommentList/>);
+});
+
+it('render a list of comments', () => {
+  const comments = [ {}, {}, {} ];
+  const wrapper = shallow(
+    <CommentList comments={comments}/>
+  );
+
+  expect(wrapper.children()).toHaveLength(comments.length);
+});
