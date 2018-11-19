84dcfd65f5a9c09a7d0c5096cb6e12acb7011154
Deniz Basegmez <dbas12@gmail.com>
2018-11-19 14:34:20 -0500

    Use morgan for logging HTTP requests

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('common'));
 
 app.get('*', (req, res) => {
   res.end(req.originalUrl);
