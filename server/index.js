356032ed48e50d396405d525a27f7c202387a676
Deniz Basegmez <dbas12@gmail.com>
2018-11-19 17:37:48 -0500

    Bootstrap mongoose

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('common'));
 
+connect()
+  .on('error', console.log)
+  .once('open', listen);
+
 app.get('*', (req, res) => {
   res.end(req.originalUrl);
 });
 
-app.listen(8080);
+function listen() {
+  app.listen(8080);
+}
+
+function connect() {
+  const options = { useNewUrlParser: true };
+  mongoose.connect('mongodb://localhost/reddit', options);
+  return mongoose.connection;
+}
