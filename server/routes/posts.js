9b1124e1b157167e1ad33723d8dbbe6ac96b6366
Deniz Basegmez <dbas12@gmail.com>
2018-11-21 16:54:04 -0500

    Reorder imports

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router = express.Router();
 const Post = require('../models/post');
+const router = express.Router();
 
 router.param('id', (req, res, next, id) => {
   Post.findById(id)
