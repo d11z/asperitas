413acfebd6420050673616f903f4023e9c6a3c5d
Deniz Basegmez <dbas12@gmail.com>
2018-11-22 16:21:22 -0500

    Add basic authorization to routes

--- server/routes.js
+++ server/routes.js
@@ -2,15 +2,18 @@ const users = require('./controllers/users');
 const posts = require('./controllers/posts');
 const comments = require('./controllers/comments');
 
+const { localAuth } = require('./auth/local');
+const { jwtAuth } = require('./auth/jwt');
+
 module.exports = app => {
-  app.post('/login', users.login);
+  app.post('/login', localAuth, users.login);
   app.post('/register', users.register);
 
   app.param('post', posts.load);
   app.get('/posts', posts.list);
-  app.post('/posts', posts.create);
-  app.delete('/posts/:post', posts.destroy);
+  app.post('/posts', jwtAuth, posts.create);
+  app.delete('/posts/:post', jwtAuth, posts.destroy);
 
-  app.post('/posts/:post', comments.create);
-  app.delete('/posts/:post/:comment', comments.destroy);
+  app.post('/posts/:post', jwtAuth, comments.create);
+  app.delete('/posts/:post/:comment', jwtAuth, comments.destroy);
 };
