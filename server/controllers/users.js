d36ef98561e4bab0c2102c55bcb468ad64bffb8a
Deniz Basegmez <dbas12@gmail.com>
2018-11-28 12:50:07 -0800

    Check if username is already taken

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,6 +9,11 @@ exports.login = (req, res) => {
 exports.register = async (req, res, next) => {
   try {
     const { username, password } = req.body;
+
+    const existingUser = await User.count({ username });
+    if (existingUser)
+      return res.status(400).json({ message: 'Username already taken' });
+
     const user = await User.create({ username, password });
     const token = createAuthToken(user.toJSON());
     res.status(201).json({ token });
