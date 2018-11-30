15af3ef603d0254c0d4e019a5dd2dc49a89eeec8
Deniz Basegmez <dbas12@gmail.com>
2018-11-30 15:04:40 -0800

    Add bad request error

--- server/routes.js
+++ server/routes.js
@@ -23,4 +23,11 @@ module.exports = app => {
   app.get('*', (req, res) => {
     res.status(404).json({ message: 'not found' });
   });
+
+  app.use((err, req, res, next) => {
+    if (err.type === 'entity.parse.failed') {
+      return res.status(400).json({ message: 'bad request' });
+    }
+    next(err);
+  });
 };
