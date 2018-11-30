0aa30a58fd3ae3bc7b4aa4b9e3a0033378d3ca46
Deniz Basegmez <dbas12@gmail.com>
2018-11-30 15:03:46 -0800

    Use lowercase error messages

--- server/auth/local.js
+++ server/auth/local.js
@@ -4,10 +4,10 @@ const User = require('../models/user');
 const localStrategy = new LocalStrategy(async (username, password, done) => {
   try {
     const user = await User.findOne({ username });
-    if (!user) return done(null, false, { message: 'User not found' });
+    if (!user) return done(null, false, { message: 'user not found' });
 
     const valid = await user.isValidPassword(password);
-    if (!valid) return done(null, false, { message: 'Invalid password' });
+    if (!valid) return done(null, false, { message: 'invalid password' });
 
     return done(null, user.toJSON());
   } catch (err) {
