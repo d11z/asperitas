e442901bccb865d84d6c3973ee9c6e105479d5c6
Deniz Basegmez <dbas12@gmail.com>
2018-11-30 19:16:36 -0800

    Simplify server connection

--- server/index.js
+++ server/index.js
@@ -6,15 +6,9 @@ const connect = url => {
   return mongoose.connect(url, config.db.options);
 };
 
-const listen = () => {
-  app.listen(config.port, () => {
-    console.log(`Listening on port ${config.port}`);
-  });
-};
-
 if (require.main === module) {
+  app.listen(config.port);
   connect(config.db.prod);
-  mongoose.connection.once('open', listen);
   mongoose.connection.on('error', console.log);
 }
 
