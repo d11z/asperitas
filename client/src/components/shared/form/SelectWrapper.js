0fdceed6984f193afe4c4012f92428c3bb8fab68
Deniz Basegmez <dbas12@gmail.com>
2019-01-13 17:46:43 -0800

    Change dropdown chevron color

--- client/src/components/shared/form/SelectWrapper.js
+++ client/src/components/shared/form/SelectWrapper.js
@@ -10,8 +10,8 @@ const SelectWrapper = styled.div`
     top: 50%;
     right: 0;
     transform: translate(-150%, calc(-50% - 2px)) rotate(45deg);
-    border-bottom: 2px solid ${props => props.theme.border};
-    border-right: 2px solid ${props => props.theme.border};
+    border-bottom: 2px solid ${props => props.theme.accent};
+    border-right: 2px solid ${props => props.theme.accent};
     width: 8px;
     height: 8px;
     pointer-events: none;
