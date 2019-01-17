d315a43c619140a44c0b7656b16612e9d6ef9f13
Deniz Basegmez <dbas12@gmail.com>
2019-01-17 14:24:48 -0800

    Add border radius to PostDetailPost

--- client/src/components/PostDetail/Post.js
+++ client/src/components/PostDetail/Post.js
@@ -3,8 +3,10 @@ import styled from 'styled-components/macro';
 import Post from '../Post';
 
 const Wrapper = styled.div`
+  overflow: hidden;
   margin-bottom: 16px;
   border: 1px solid ${props => props.theme.border};
+  border-radius: 2px;
   
   @media (max-width: 768px) {
     margin-bottom: 0;
