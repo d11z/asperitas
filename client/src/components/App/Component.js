656a88ff82b4b305a83a25fb50b3d9838f0888fa
Deniz Basegmez <dbas12@gmail.com>
2019-02-08 19:30:36 -0800

    Scroll to top on route change

--- client/src/components/App/Component.js
+++ client/src/components/App/Component.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Router, Route, Switch } from 'react-router-dom';
 import theme from '../../theme';
 import history from '../../util/history';
+import AppScrollToTop from './ScrollToTop';
 import GlobalStyle from '../../globalStyle';
 import HeaderContainer from '../Header/Container';
 import ErrorNotificationContainer from '../ErrorNotification/Container';
@@ -8,13 +11,11 @@ import LoginFormContainer from '../LoginForm/Container';
 import SignupFormContainer from '../SignupForm/Container';
 import CreatePostFormContainer from '../CreatePostForm/Container';
 import Home from '../Home';
-import { ThemeProvider } from 'styled-components';
-import { Router, Route, Switch } from 'react-router-dom';
 
 const App = props => (
   <ThemeProvider theme={theme(props.dark)}>
     <Router history={history}>
-      <>
+      <AppScrollToTop>
         <GlobalStyle />
         <Route component={HeaderContainer} />
         <Route component={ErrorNotificationContainer} />
@@ -24,7 +25,7 @@ const App = props => (
           <Route path='/createpost' component={CreatePostFormContainer} />
           <Route path='/' component={Home} />
         </Switch>
-      </>
+      </AppScrollToTop>
     </Router>
   </ThemeProvider>
 );
