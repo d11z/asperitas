5d695092dd82745cd91727fc72d6d506a400031f
Deniz Basegmez <dbas12@gmail.com>
2019-02-08 20:23:04 -0800

    Use react-scroll-manager for proper scroll restoration

--- client/src/components/App/Component.js
+++ client/src/components/App/Component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ScrollManager, WindowScroller } from 'react-scroll-manager';
 import { Router, Route, Switch } from 'react-router-dom';
 import theme from '../../theme';
 import history from '../../util/history';
-import AppScrollToTop from './ScrollToTop';
 import GlobalStyle from '../../globalStyle';
 import HeaderContainer from '../Header/Container';
 import ErrorNotificationContainer from '../ErrorNotification/Container';
@@ -14,19 +14,21 @@ import Home from '../Home';
 
 const App = props => (
   <ThemeProvider theme={theme(props.dark)}>
-    <Router history={history}>
-      <AppScrollToTop>
-        <GlobalStyle />
-        <Route component={HeaderContainer} />
-        <Route component={ErrorNotificationContainer} />
-        <Switch>
-          <Route path='/login' component={LoginFormContainer} />
-          <Route path='/signup' component={SignupFormContainer} />
-          <Route path='/createpost' component={CreatePostFormContainer} />
-          <Route path='/' component={Home} />
-        </Switch>
-      </AppScrollToTop>
-    </Router>
+    <ScrollManager history={history}>
+      <Router history={history}>
+        <WindowScroller>
+          <GlobalStyle />
+          <Route component={HeaderContainer} />
+          <Route component={ErrorNotificationContainer} />
+          <Switch>
+            <Route path='/login' component={LoginFormContainer} />
+            <Route path='/signup' component={SignupFormContainer} />
+            <Route path='/createpost' component={CreatePostFormContainer} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </WindowScroller>
+      </Router>
+    </ScrollManager>
   </ThemeProvider>
 );
 
